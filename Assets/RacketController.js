d7bbed3477430689e98bc19a295d5339067fd488
jbrmaiden <jordanboaz14@gmail.com>
2014-09-08 18:50:08 -0400

    First commit

---
+++ Assets/RacketController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	//ResetRotation();
+	//Debug.Log( transform.eulerAngles.y);
+	if(Input.GetKey(KeyCode.LeftArrow)){
+		animation.Play("Racket Moviment Left");
+	}
+
+}
+
+function ResetRotation(){
+	while( transform.eulerAngles.y > 0){
+		transform.eulerAngles.y += -20;
+	}
+	yield;
+}
+
+function MoveRotation(){
+	while( transform.eulerAngles.y < 53){
+		transform.eulerAngles.y += 20;
+	}
+	yield;
+}
